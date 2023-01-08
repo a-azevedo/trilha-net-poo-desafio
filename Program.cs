656760c2dd6e952072382201acc74744e066ec5c
a-azevedo <aelmajan.azevedo@gmail.com>
2023-01-08 19:14:24 -0300

    Implementado todas as novas funcionalidades

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Implementado: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("1234", "Iphone-14", "imei-8908", 128);
+Nokia nokia = new Nokia("4321", "Nokia-5310", "imei-4561", 128);
+
+Console.WriteLine("Smartphone Iphone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Reddit");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Nubank");
